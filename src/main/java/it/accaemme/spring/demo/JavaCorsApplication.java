023114215f939f8a5e641eaadd3f58234cf02ee1
AccaEmme <acca.m@hotmail.it>
2021-08-06 21:44:04 +0200

    init commit

---
+++ src/main/java/it/accaemme/spring/demo/JavaCorsApplication.java
@@ -0,0 +1,13 @@
+package it.accaemme.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCorsApplication.class, args);
+	}
+
+}
